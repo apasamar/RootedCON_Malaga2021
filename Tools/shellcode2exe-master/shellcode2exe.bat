6e5bc9bea9b8d3bcde993ac0638ea2bee88d361a
Abraham Pasamar <apasamar@incide.es>
2021-12-13 18:48:36 +0100

    Add files via upload

---
+++ Tools/shellcode2exe-master/shellcode2exe.bat
@@ -0,0 +1,21 @@
+@echo off
+@echo Global Start > shellcode.asm
+@echo SECTION 'foo' write, execute,read >> shellcode.asm
+@echo Start:       >> shellcode.asm
+@echo incbin "%2"  >> shellcode.asm
+@yasm.exe -f win%1 -o shellcode.obj shellcode.asm
+@golink /ni /fo %3 /entry Start shellcode.obj
+:: @del shellcode.asm
+:: @del shellcode.obj
+@dir %3*
+
+@goto exit
+
+@:help
+@echo Converts a shellcode blob to an executable
+@echo Required Arguments:
+@echo - architecture: 32 or 64 (depending on the shellcode)
+@echo - shellcode blob file name
+@echo - Output executable file name
+@:exit
+echo.
